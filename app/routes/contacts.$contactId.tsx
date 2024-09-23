a7ace20a2c72bf65caafb105dbaac85df81fda26
Andrii Bandura <bbanduraa18@gmail.com>
2024-09-23 15:25:18 +0200

    feat(contact): added checking that contact exists

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,12 +1,19 @@
 import { Form, useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import type { FunctionComponent } from "react";
+import invariant from "tiny-invariant";
 
 import { getContact } from "../data";
 import type { ContactRecord } from "../data";
 
-export const loader = async ({ params }) => {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
   const contact = await getContact(params.contactId);
+
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
   return json({ contact });
 };
 
