cefc1144eb326ec162ff72cf2ac56aa68e5849d3
Andrii Bandura <bbanduraa18@gmail.com>
2024-09-23 15:39:12 +0200

    feat(edit contact): implement edit page & action

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ import { json } from "@remix-run/node";
 import type { LinksFunction } from "@remix-run/node";
 
 import appStylesHref from "./app.css?url";
-import { getContacts } from "./data";
+import { getContacts, createEmptyContact } from "./data";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
@@ -21,6 +21,11 @@ export const links: LinksFunction = () => [
 export const loader = async () => {
   const contacts = await getContacts();
   return json({ contacts });
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
 };
 
 export default function App() {
